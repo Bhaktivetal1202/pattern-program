c3b94369062d812fbe6d9c17fd662d96e6dcd3c1
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-09-09 19:33:24 +0530

    Add files via upload

---
+++ patterrn 6.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int row,col;
+	for(row=1;row<=4;row++)
+	{
+		for(col=1;col<=row;col++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(row=3;row>=1;row--)
+	{
+		for(col=1;col<=row;col++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
